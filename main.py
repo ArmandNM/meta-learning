c6addd9e5d2832c4eff9c3b560caf1da74bbe177
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2020-02-26 00:19:16 +0200

    Implemented baseline model described in Vinyals et al 2016 and tested training it on one batch sampled with Torhmeta

---
+++ main.py
@@ -0,0 +1,75 @@
+import torch
+import higher
+import torchmeta
+
+from torchmeta.datasets.helpers import miniimagenet
+from torchmeta.utils.data import BatchMetaDataLoader
+
+from time import time
+
+from models.MetaConv import MetaConv
+
+
+def mock_train(train_inputs, train_labels):
+    model = MetaConv(in_size=train_inputs.size(2),
+                     in_channels=train_inputs.size(1),
+                     out_channels=train_labels.size(0))
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    model.train()
+
+    running_loss = 0.0
+    running_accuracy = 0.0
+    start = time()
+
+    print_step = 50
+
+    for i in range(1000):
+        optimizer.zero_grad()
+
+        outputs = model(train_inputs)
+
+        loss = criterion(outputs, train_labels)
+        loss.backward()
+        optimizer.step()
+
+        _, predicted = torch.max(outputs, dim=1)
+        accuracy = (predicted == train_labels).sum().item() / train_labels.size(0)
+
+        running_loss += loss
+        running_accuracy += accuracy
+
+        if i % print_step == 0:
+            end = time()
+            print(f'Iteration {i} loss: {running_loss / 1000 :.10f} '
+                  f'accuracy: {100 * accuracy}% '
+                  f'speed: {print_step / (end - start) :.2f} iter/s')
+            running_loss = 0.0
+            running_accuracy = 0.0
+            start = time()
+
+
+def main():
+    print(torch.cuda.is_available())
+    print(torchmeta.__version__)
+
+    dataset = miniimagenet('datasets', ways=5, shots=1, test_shots=15, meta_train=True, download=True)
+    dataloader = BatchMetaDataLoader(dataset, batch_size=16, num_workers=4)
+
+    for batch in dataloader:
+        train_inputs, train_labels = batch["train"]
+        print(f'Train inputs shape: {train_inputs.shape}')
+        print(f'Train labels shape: {train_labels.shape}')
+
+        test_inputs, test_labels = batch["test"]
+        print(f'Test inputs shape: {test_inputs.shape}')
+        print(f'Test labels shape: {test_labels.shape}')
+
+        mock_train(train_inputs[0], train_labels[0])
+
+        break
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/MetaConv.py
@@ -0,0 +1,38 @@
+import torch
+
+
+def conv_block(in_channels, out_channels):
+    block = torch.nn.Sequential(torch.nn.Conv2d(in_channels=in_channels,
+                                                out_channels=out_channels,
+                                                kernel_size=3,
+                                                padding=1),
+                                torch.nn.BatchNorm2d(num_features=out_channels,
+                                                     momentum=1.0,
+                                                     track_running_stats=True),
+                                torch.nn.ReLU(),
+                                torch.nn.MaxPool2d(2))
+    return block
+
+
+class MetaConv(torch.nn.Module):
+    def __init__(self, in_size=84, in_channels=3, out_channels=5, hidden_size=32):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_size = hidden_size
+
+        self.net = torch.nn.Sequential(conv_block(in_channels, hidden_size),
+                                       conv_block(hidden_size, hidden_size),
+                                       conv_block(hidden_size, hidden_size),
+                                       conv_block(hidden_size, hidden_size),
+                                       Flatten(),
+                                       torch.nn.Linear(int(in_size / 16) ** 2 * hidden_size, out_channels))
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Flatten(torch.nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
