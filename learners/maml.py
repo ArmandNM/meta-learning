e0c2f2fe7868d33e67bd3e4e697de656e4793f1a
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2020-03-03 17:37:57 +0200

    [refactor] bootstrapped meta learning process to support any learner

--- learners/maml.py
+++ learners/maml.py
@@ -1,192 +1,96 @@
 import torch
 import higher
 import os
+import argparse
 
-from torchmeta.datasets.helpers import miniimagenet
-from torchmeta.utils.data import BatchMetaDataLoader
-
-from torch.optim.optimizer import Optimizer
-from time import time
+from models.metaconv import MetaConv
 
 
 class MAML:
-    def __init__(self, train_dataloader, val_dataloader, test_dataloader, model: torch.nn.Module, optimizer: Optimizer,
-                 meta_batch_size, ways, shots, test_shots, inner_steps_train, inner_steps_test, outer_steps,
-                 experiment_name):
-        self.train_dataloader = train_dataloader
-        self.val_dataloader = val_dataloader
-        self.test_dataloader = test_dataloader
-        self.model = model
-        self.optimizer = optimizer
-        self.meta_batch_size = meta_batch_size
-        self.n_way = ways
-        self.k_spt = shots
-        self.k_qry = test_shots
-        self.inner_steps_train = inner_steps_train
-        self.inner_steps_test = inner_steps_test
-        self.outer_steps = outer_steps
-        self.experiment_name = experiment_name
+    def __init__(self, args):
+        self.args = args
+
+        self.model = None
+        if args.model == 'meta_conv':
+            self.model = MetaConv(out_channels=self.args.n_ways)
+            self.model.cuda()
+        assert self.model is not None
 
         self.best_model = None
 
-    def train(self, training=False, validation=False, testing=False, resume=False, train_iter=None):
-        # Check that exactly one of training/validating/testing parameters is set to True
-        assert int(training) + int(validation) + int(testing) == 1
-        # Check that resume is True only if training/testing in True
-        # assert int(training) ^ int(resume) == 0 or resume is False
+    @staticmethod
+    def add_arguments(argparser: argparse.ArgumentParser):
+        argparser.add_argument('--inner_steps_train', type=int, help='number of iters in inner loop ar train time', default=5)
+        argparser.add_argument('--inner_steps_test', type=int, help='number of iters in inner loop ar test time', default=10)
+        argparser.add_argument('--model', type=str, help='model optimized in inner loop', default='meta_conv')
 
-        # Select dataset split
+    def get_trainable_params(self):
+        return self.model.parameters()
+
+    def set_train_mode(self):
+        self.model.train()
+
+    def set_eval_mode(self):
+        self.model.eval()
+
+    def run_iteration(self, meta_batch, training=False):
         if training:
             self.model.train()
-            dataloader = self.train_dataloader
-            inner_steps = self.inner_steps_train
-        if validation:
+            inner_steps = self.args.inner_steps_train
+        else:
             self.model.eval()
-            dataloader = self.val_dataloader
-            inner_steps = self.inner_steps_test
-        if testing:
-            if resume:
-                self.load_checkpoint(checkpoint_name='best_checkpoint')
-            self.model.eval()
-            dataloader = self.test_dataloader
-            inner_steps = self.inner_steps_test
+            inner_steps = self.args.inner_steps_test
 
-        running_loss = 0.0
-        running_accuracy = 0.0
-        train_print_step = 10
-        val_print_step = 50
-        test_num_iters = 50
-        if resume:
-            test_num_iters = 1000
-        checkpoint_step = 500
-        start = time()
+        meta_batch_loss = 0.0
+        meta_batch_accuracy = 0.0
 
-        for it, meta_batch in enumerate(dataloader):
-            # Stop conditions
-            if training and it > self.outer_steps:
-                break
-            if validation and it > val_print_step:
-                break
-            if testing and it > test_num_iters:
-                break
+        meta_train_inputs, meta_train_labels = meta_batch["train"]
+        meta_test_inputs, meta_test_labels = meta_batch["test"]
 
-            # Load immediately after saving model only for developing purposes
-            if training and it > 0 and (it - 1) % checkpoint_step == 0:
-                self.load_checkpoint(checkpoint_name=f'checkpoint_{it - 1}')
+        inner_optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-2)
 
-            meta_train_inputs, meta_train_labels = meta_batch["train"]
-            meta_test_inputs, meta_test_labels = meta_batch["test"]
+        for task_idx in range(self.args.tasks_num):
+            # Create inner loop context using higher library
+            with higher.innerloop_ctx(self.model, opt=inner_optimizer,
+                                      copy_initial_weights=False, track_higher_grads=training) as (fmodel, diffopt):
 
-            inner_optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-2)
+                # Extract examples and labels for current task
+                train_inputs = meta_train_inputs[task_idx].cuda()
+                train_labels = meta_train_labels[task_idx].cuda()
 
-            # Only care about meta-optimizer at meta-training time
-            if training:
-                self.optimizer.zero_grad()
+                test_inputs = meta_test_inputs[task_idx].cuda()
+                test_labels = meta_test_labels[task_idx].cuda()
 
-            # Run the inner loop for all tasks in meta-batch
-            for task_idx in range(self.meta_batch_size):
-                # Create inner loop context using higher library
-                with higher.innerloop_ctx(self.model, opt=inner_optimizer,
-                                          copy_initial_weights=False, track_higher_grads=training) as (fmodel, diffopt):
+                # Inner loop
+                for _ in range(inner_steps):
+                    train_logits = fmodel(train_inputs)
+                    train_loss = torch.nn.functional.cross_entropy(train_logits, train_labels)
 
-                    # Extract examples and labels for current task
-                    train_inputs = meta_train_inputs[task_idx].cuda()
-                    train_labels = meta_train_labels[task_idx].cuda()
+                    # _, train_predictions = torch.max(train_logits, dim=1)
+                    # train_accuracy = (train_predictions == train_labels).sum().item() / train_labels.size(0)
+                    # print(train_accuracy)
 
-                    test_inputs = meta_test_inputs[task_idx].cuda()
-                    test_labels = meta_test_labels[task_idx].cuda()
+                    diffopt.step(train_loss)
 
-                    # Inner loop
-                    for _ in range(inner_steps):
-                        train_logits = fmodel(train_inputs)
-                        train_loss = torch.nn.functional.cross_entropy(train_logits, train_labels)
+                # Query the trained model
+                test_logits = fmodel(test_inputs)
+                test_loss = torch.nn.functional.cross_entropy(test_logits, test_labels)
+                _, test_predictions = torch.max(test_logits, dim=1)
 
-                        # _, train_predictions = torch.max(train_logits, dim=1)
-                        # train_accuracy = (train_predictions == train_labels).sum().item() / train_labels.size(0)
-                        # print(train_accuracy)
+                test_accuracy = (test_predictions == test_labels).sum().item() / test_labels.size(0)
 
-                        diffopt.step(train_loss)
+                # Compute metrics
+                meta_batch_loss += test_loss.detach()
+                meta_batch_accuracy += test_accuracy
 
-                    # Query the trained model
-                    test_logits = fmodel(test_inputs)
-                    test_loss = torch.nn.functional.cross_entropy(test_logits, test_labels)
-                    _, test_predictions = torch.max(test_logits, dim=1)
-
-                    test_accuracy = (test_predictions == test_labels).sum().item() / test_labels.size(0)
-
-                    # Compute metrics
-                    running_loss += test_loss.detach()
-                    running_accuracy += test_accuracy
-
-                    # Propagate task loss through inner loop rollup
-                    if training:
-                        test_loss.backward()
-
-            # Use the meta-optimizer to update parameters
-            if training:
-                self.optimizer.step()
-
-            # Print intermediate results for training or val/test results
-            if (training and it % train_print_step == 0) or (validation and it == val_print_step)\
-                    or (testing and it == test_num_iters):
-                end = time()
-
+                # Propagate task loss through inner loop rollup
                 if training:
-                    print_step = train_print_step
-                    phase = '[TRAIN]'
-                if validation:
-                    print_step = val_print_step + 1
-                    phase = '[VAL]'
-                if testing:
-                    print_step = test_num_iters + 1
-                    phase = '[TEST]'
+                    test_loss.backward()
 
-                print(f'{phase} Iteration {it} loss: {running_loss / (self.meta_batch_size * print_step) :.5f} '
-                      f'accuracy: {100 * running_accuracy / (self.meta_batch_size * print_step) :.2f}% '
-                      f'speed: {print_step / (end - start) :.2f} iter/s')
+        return meta_batch_loss, meta_batch_accuracy
 
-                # [TODO] Save best model based on val accuracy
-                if validation and (self.best_model is None or
-                                   100 * running_accuracy / (self.meta_batch_size * print_step) > self.best_model):
-                    self.best_model = 100 * running_accuracy / (self.meta_batch_size * print_step)
-                    self.save_checkpoint(it=train_iter, checkpoint_name='best_checkpoint')
+    def get_state_dict(self):
+        return {'model_state_dict': self.model.state_dict()}
 
-                running_loss = 0.0
-                running_accuracy = 0.0
-
-                start = time()
-
-            # Run validation to select the best model
-            if training and it > int(0.0000005 * self.outer_steps) and it % val_print_step == 0:
-                self.model.eval()
-                self.eval(train_iter=it)
-                self.test(resume=False)
-                # Model must be set back to training mode after an evaluation
-                self.model.train()
-                start = time()
-
-            # Save checkpoint of model and optimizer
-            if training and it % checkpoint_step == 0:
-                self.save_checkpoint(it=it, checkpoint_name=f'checkpoint_{it}')
-
-    def eval(self, train_iter=None):
-        self.train(validation=True, train_iter=train_iter)
-
-    def test(self, resume):
-        self.train(testing=True, resume=resume)
-
-    def save_checkpoint(self, it, checkpoint_name):
-        if not os.path.isdir(f'experiments/{self.experiment_name}'):
-            os.makedirs(f'experiments/{self.experiment_name}')
-        print(f'Saving {checkpoint_name} ...')
-        torch.save({
-            'iteration': it,
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict()
-        }, f'experiments/{self.experiment_name}/{checkpoint_name}')
-
-    def load_checkpoint(self, checkpoint_name):
-        checkpoint = torch.load(f'experiments/{self.experiment_name}/{checkpoint_name}')
-        print(f'Loading {checkpoint_name} from iteration {checkpoint["iteration"]} ...')
-        self.model.load_state_dict(checkpoint['model_state_dict'])
-        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    def load_checkpoint(self, state_dict):
+        self.model.load_state_dict(state_dict['model_state_dict'])
---
+++ learners/meta_trainer.py
@@ -0,0 +1,208 @@
+import torch
+import argparse
+import os
+
+from learners.maml import MAML
+
+from torchmeta.datasets.helpers import miniimagenet
+from torchmeta.utils.data import BatchMetaDataLoader
+
+from time import time
+
+
+class MetaTrainer:
+    def __init__(self):
+        self.argparser = argparse.ArgumentParser()
+
+        self.argparser.add_argument('--n_ways', type=int, help='num of classes', default=5)
+        self.argparser.add_argument('--k_spt', type=int, help='samples per class in support set', default=1)
+        self.argparser.add_argument('--k_qry', type=int, help='samples per class in query set', default=15)
+        self.argparser.add_argument('--tasks_num', type=int, help='meta batch size', default=5)
+        self.argparser.add_argument('--meta_learner', type=str, help='name of meta learning method', default='maml')
+        self.argparser.add_argument('--meta_optimizer', type=str, help='meta optimizer', default='adam')
+        self.argparser.add_argument('--meta_iters', type=int, help='num of meta iterations', default=150)
+        self.argparser.add_argument('--meta_lr', type=float, help='meta learning rate', default=1e-3)
+        self.argparser.add_argument('--dataset', type=str, help='meta dataset to use', default='miniimagenet')
+        self.argparser.add_argument('--experiment_name', type=str, help='name of current experiment', default='maml')
+
+        self.argparser.add_argument('--train_print_step', type=int,
+                                    help='number of meta iterations before printing metrics', default=10)
+        self.argparser.add_argument('--val_print_step', type=int,
+                                    help='number of meta iterations before running validation', default=50)
+        self.argparser.add_argument('--val_num_iters', type=int,
+                                    help='number of meta iterations to run for validation', default=50)
+        self.argparser.add_argument('--test_num_iters', type=int,
+                                    help='number of meta iterations to run for testing', default=1000)
+        self.argparser.add_argument('--checkpoint_step', type=str,
+                                    help='number of meta iterations before saving checkpoint', default=500)
+
+        # Parse general meta-learning args
+        self.args, additional_args = self.argparser.parse_known_args()
+
+        # Selected meta-learner constructor
+        learner_constructor = None
+        print(self.args)
+        if self.args.meta_learner == 'maml':
+            learner_constructor = MAML
+        assert learner_constructor is not None
+
+        # Parse specific parameters of the selected meta-learner
+        learner_argparser = argparse.ArgumentParser()
+        learner_constructor.add_arguments(argparser=learner_argparser)
+        self.args, _ = learner_argparser.parse_known_args(additional_args, namespace=self.args)
+        print(self.args)
+
+        # Create dataset loaders
+        dataset = None
+        if self.args.dataset == 'miniimagenet':
+            dataset = miniimagenet
+        assert dataset is not None
+
+        train_dataset = dataset('datasets', ways=self.args.n_ways, shots=self.args.k_spt, test_shots=self.args.k_qry,
+                                meta_train=True, download=True)
+        self.train_dataloader = BatchMetaDataLoader(train_dataset, batch_size=self.args.tasks_num, num_workers=4)
+
+        val_dataset = dataset('datasets', ways=self.args.n_ways, shots=self.args.k_spt, test_shots=self.args.k_qry,
+                              meta_train=True, download=True)
+        self.val_dataloader = BatchMetaDataLoader(val_dataset, batch_size=self.args.tasks_num, num_workers=4)
+
+        test_dataset = dataset('datasets', ways=self.args.n_ways, shots=self.args.k_spt, test_shots=self.args.k_qry,
+                               meta_train=True, download=True)
+        self.test_dataloader = BatchMetaDataLoader(test_dataset, batch_size=self.args.tasks_num, num_workers=4)
+
+        # Create meta-learner object
+        self.learner = learner_constructor(args=self.args)
+
+        # Create optimizer
+        self.optimizer = None
+        if self.args.meta_optimizer == 'adam':
+            self.optimizer = torch.optim.Adam(params=self.learner.get_trainable_params(), lr=self.args.meta_lr)
+        assert self.optimizer is not None
+
+        # Used to save best checkpoint, initially None
+        self.best_score = None
+
+    def train(self, training=False, validation=False, testing=False, resume=False, train_iter=None):
+        # Check that exactly one of training/validation/testing parameters is set to True
+        assert int(training) + int(validation) + int(testing) == 1
+
+        # Select dataset split and stopping conditions based on training/validation/testing
+        dataloader = None
+        num_meta_iterations = None
+        if training:
+            num_meta_iterations = self.args.meta_iters
+            dataloader = self.train_dataloader
+        elif validation:
+            num_meta_iterations = self.args.val_num_iters
+            dataloader = self.val_dataloader
+        elif testing:
+            num_meta_iterations = self.args.val_num_iters
+            if resume:
+                self.load_checkpoint(checkpoint_name='best_checkpoint')
+                num_meta_iterations = self.args.test_num_iters
+            dataloader = self.test_dataloader
+        assert dataloader is not None
+        assert num_meta_iterations is not None
+
+        running_loss = 0.0
+        running_accuracy = 0.0
+        start = time()
+
+        for it, meta_batch in enumerate(dataloader):
+            # Stop conditions
+            if it > num_meta_iterations:
+                break
+
+            # Load immediately after saving model only for developing purposes
+            if training and it > 0 and (it - 1) % self.args.checkpoint_step == 0:
+                self.load_checkpoint(checkpoint_name=f'checkpoint_{it - 1}')
+
+            # Only care about meta-optimizer at meta-training time
+            if training:
+                self.optimizer.zero_grad()
+
+            # Run one iteration and get resulting metrics
+            loss, accuracy = self.learner.run_iteration(meta_batch=meta_batch, training=training)
+            # print(f'res loss {loss}')
+            # print(f'res acc {accuracy}')
+
+            # Update running metrics
+            running_loss += loss
+            running_accuracy += accuracy
+
+            # Update learnable parameters
+            if training:
+                self.optimizer.step()
+
+            # Print intermediate results for training or val/test results
+            if (training and it % self.args.train_print_step == 0) or (not training and it == num_meta_iterations):
+                end = time()
+
+                if training:
+                    print_step = self.args.train_print_step
+                    phase = '[TRAIN]'
+                if validation:
+                    print_step = num_meta_iterations + 1
+                    phase = '[VAL]'
+                if testing:
+                    print_step = num_meta_iterations + 1
+                    phase = '[TEST]'
+
+                print(f'{phase} Iteration {it} loss: {running_loss / (self.args.tasks_num * print_step) :.5f} '
+                      f'accuracy: {100 * running_accuracy / (self.args.tasks_num * print_step) :.2f}% '
+                      f'speed: {print_step / (end - start) :.2f} iter/s')
+
+                # Save best model based on val accuracy
+                if validation and (self.best_score is None or
+                                   100 * running_accuracy / (self.args.tasks_num * print_step) > self.best_score):
+                    self.best_score = 100 * running_accuracy / (self.args.tasks_num * print_step)
+                    self.save_checkpoint(it=train_iter, checkpoint_name='best_checkpoint')
+
+                running_loss = 0.0
+                running_accuracy = 0.0
+
+                start = time()
+
+            # Run validation to select the best model
+            if training and it > int(0.0000005 * self.args.meta_iters) and it % self.args.val_print_step == 0:
+                self.learner.set_eval_mode()
+                self.eval(train_iter=it)
+                self.test(resume=False)
+                # Model must be set back to training mode after an evaluation
+                # [TODO] implement and set_train_mode() for the learner instead of accessing member
+                self.learner.set_train_mode()
+                start = time()
+
+            # Save checkpoint of model and optimizer
+            if training and it % self.args.checkpoint_step == 0:
+                self.save_checkpoint(it=it, checkpoint_name=f'checkpoint_{it}')
+
+    def eval(self, train_iter=None):
+        self.learner.set_eval_mode()
+        self.train(validation=True, train_iter=train_iter)
+
+    def test(self, resume=False):
+        self.learner.set_train_mode()
+        self.train(testing=True, resume=resume)
+
+    def save_checkpoint(self, it, checkpoint_name):
+        if not os.path.isdir(f'experiments/{self.args.experiment_name}'):
+            os.makedirs(f'experiments/{self.args.experiment_name}')
+        print(f'Saving {checkpoint_name} ...')
+        torch.save({
+            'iteration': it,
+            'learner_state_dict': self.learner.get_state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict()
+        }, f'experiments/{self.args.experiment_name}/{checkpoint_name}')
+
+    def load_checkpoint(self, checkpoint_name):
+        checkpoint = torch.load(f'experiments/{self.args.experiment_name}/{checkpoint_name}')
+        print(f'Loading {checkpoint_name} from iteration {checkpoint["iteration"]} ...')
+        self.learner.load_checkpoint(state_dict=checkpoint['learner_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+
+if __name__ == '__main__':
+    meta_trainer = MetaTrainer()
+    meta_trainer.train(training=True)
+    meta_trainer.test(resume=True)
