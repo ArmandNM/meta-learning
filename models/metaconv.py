9822f645e5cbcc9cc71118cfcc003287570bba03
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2020-03-20 20:51:44 +0200

    Reimplemented MetaConv in the traditional way to get rid of the sequencial module

--- models/metaconv.py
+++ models/metaconv.py
@@ -1,17 +1,19 @@
 import torch
+import torch.nn.functional as F
 
 
-def conv_block(in_channels, out_channels):
-    block = torch.nn.Sequential(torch.nn.Conv2d(in_channels=in_channels,
-                                                out_channels=out_channels,
-                                                kernel_size=3,
-                                                padding=1),
-                                torch.nn.BatchNorm2d(num_features=out_channels,
-                                                     momentum=1.0,
-                                                     track_running_stats=False),
-                                torch.nn.ReLU(),
-                                torch.nn.MaxPool2d(2))
-    return block
+class ConvBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv2d = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        self.batchnorm2d = torch.nn.BatchNorm2d(num_features=out_channels, momentum=1.0, track_running_stats=False)
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        x = self.batchnorm2d(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        return x
 
 
 class MetaConv(torch.nn.Module):
@@ -22,17 +24,18 @@ class MetaConv(torch.nn.Module):
         self.out_channels = out_channels
         self.hidden_size = hidden_size
 
-        self.net = torch.nn.Sequential(conv_block(in_channels, hidden_size),
-                                       conv_block(hidden_size, hidden_size),
-                                       conv_block(hidden_size, hidden_size),
-                                       conv_block(hidden_size, hidden_size),
-                                       Flatten(),
-                                       torch.nn.Linear(int(in_size / 16) ** 2 * hidden_size, out_channels))
+        self.block1 = ConvBlock(in_channels, hidden_size)
+        self.block2 = ConvBlock(hidden_size, hidden_size)
+        self.block3 = ConvBlock(hidden_size, hidden_size)
+        self.block4 = ConvBlock(hidden_size, hidden_size)
+
+        self.fc = torch.nn.Linear(int(in_size / 16) ** 2 * hidden_size, out_channels)
 
     def forward(self, x):
-        return self.net(x)
-
-
-class Flatten(torch.nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
