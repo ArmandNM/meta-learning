e038e83858913cfdb74cb5f1cc959107229fc830
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2020-04-12 23:50:21 +0300

    removed datasets from project backup

--- start_experiment.sh
+++ start_experiment.sh
@@ -19,9 +19,13 @@ mkdir "experiments/${EXPERIMENT_NAME}"
 # Clone project directory
 echo "Cloning project to experiment directory"
 mkdir "$EXPERIMENT_PATH/project/" 2> /dev/null
-shopt -s extglob
-cp -r !("experiments") "$EXPERIMENT_PATH/project/"
+#shopt -s extglob
+#cp -r !("experiments", "datasets") "$EXPERIMENT_PATH/project/"
+rsync -a ./ "$EXPERIMENT_PATH/project/" --exclude "experiments" --exclude "datasets"
+mkdir "$EXPERIMENT_PATH/project/experiments"
+ln -s "$(pwd)/datasets" "$(pwd)/$EXPERIMENT_PATH/project/datasets"
 echo "Starting experiment: $EXPERIMENT_NAME"
 
 # Start experiment in the background
-nohup python main.py --experiment_name="${EXPERIMENT_NAME}" > "$EXPERIMENT_PATH/${EXPERIMENT_NAME}.log" 2>&1 &
+nohup python main.py --training="yes" --experiment_name="${EXPERIMENT_NAME}" > "$EXPERIMENT_PATH/${EXPERIMENT_NAME}.log" 2>&1 &
+tail -f "$EXPERIMENT_PATH/${EXPERIMENT_NAME}.log"
